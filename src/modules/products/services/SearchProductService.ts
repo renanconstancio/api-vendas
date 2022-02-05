f26e63ea6548e710ec942745b98ebe19cb51deba
renanconstancio <renanhconstancio@gmail.com>
2022-02-05 14:39:42 -0300

    ajuste

---
+++ src/modules/products/services/SearchProductService.ts
@@ -0,0 +1,25 @@
+import { inject, injectable } from 'tsyringe';
+import AppError from '@shared/errors/AppError';
+import { IProductsRepository } from '../domain/repositories/IProductsRepository';
+import { ISearchProduct } from '../domain/models/ISearchProduct';
+import { IProductPaginate } from '../domain/models/IProductPaginate';
+
+@injectable()
+class SearchProductService {
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
+
+  public async execute({ name }: ISearchProduct): Promise<IProductPaginate> {
+    const product = await this.productsRepository.searchAllPaginate(name);
+
+    if (!product) {
+      throw new AppError('Product not found.');
+    }
+
+    return product;
+  }
+}
+
+export default SearchProductService;
